c8d8d944dc3ffe58bbe3521f5c9b1909235c1b18
kurihara <find.chown.ls.mtime.4@gmail.com>
2023-06-20 15:22:57 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,36 @@
+#import pandas as pd
+from pprint import pprint
+
+def func1(x:int, y:int)->int:
+    """_summary_
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        str: _description_
+    """
+    return x+y
+
+
+print("hello")
+
+print(func1(1,2))
+
+test=["1111",
+      "2222",
+      "3333",
+      ]
+print(test)
+
+ary = {}
+ary["zero"] = 'a'
+ary["one"] = 'b'
+ary["two"] = 'c'
+
+print(ary)
+
+print("Conflict-test-master")
+
+print("add print")
